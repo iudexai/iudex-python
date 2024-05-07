c341c56ca061705a3f81a943be8baa381639f032
Arno Gau <tetranoir@gmail.com>
2024-05-07 12:48:07 -0700

    add lambda instructions

--- iudex/instrumentation/README.md
+++ iudex/instrumentation/README.md
@@ -20,3 +20,20 @@ instrument_app(app=app, api_key=os.getenv("IUDEX_API_KEY"), service_name=__main_
 3. Make sure the app has access to the environment variable `IUDEX_API_KEY`
 4. You should be all set! Go to [https://app.iudex.ai/](https://app.iudex.ai/) and enter your API key
 5. Go to [https://app.iudex.ai/logs](https://app.iudex.ai/logs) and press `Search` to view your logs
+
+
+# Lambda / Serverless
+
+1. Pip install dependencies
+```bash
+pip install iudex
+```
+2. Import `instrument_app` where you defined FastAPI (usually `main.py`) from `iudex`
+```python
+# Add this in your lambda function file (likely lambda_function.py)
+from iudex.instrumentation.lambda_instrumentation import instrument_app
+instrument_app(api_key=os.getenv("IUDEX_API_KEY"), service_name=__main__)
+```
+3. Make sure the app has access to the environment variable `IUDEX_API_KEY`
+4. You should be all set! Go to [https://app.iudex.ai/](https://app.iudex.ai/) and enter your API key
+5. Go to [https://app.iudex.ai/logs](https://app.iudex.ai/logs) and press `Search` to view your logs
--- iudex/instrumentation/lambda_instrumentation.py
+++ iudex/instrumentation/lambda_instrumentation.py
@@ -1,26 +1,30 @@
 import logging
 import os
 
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
 from opentelemetry._logs import set_logger_provider
 from opentelemetry.exporter.otlp.proto.http._log_exporter import OTLPLogExporter
 from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
 from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
 from opentelemetry.sdk.resources import Resource
 
-endpoint = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT") or "https://hsyrgkn5ja.execute-api.us-west-2.amazonaws.com/resource_logs"
+endpoint = os.getenv("OTEL_EXPORTER_OTLP_ENDPOINT") or "https://5pz08znmzj.execute-api.us-west-2.amazonaws.com/resource_logs"
 env_api_key = os.getenv("IUDEX_API_KEY")
 
 configured = False
 
-def configure(service_name: str | None, instance_id: str | None, api_key: str | None = env_api_key):
+def configure(
+    service_name: str | None,
+    instance_id: str | None,
+    api_key: str | None = env_api_key,
+    endpoint: str | None = endpoint
+):
     global configured
     if (configured):
         return
-    resource = Resource.create({
-        "service.name": service_name,
-        "service.instance.id": instance_id,
-    })
+    attributes = {}
+    if (service_name): attributes["service.name"] = service_name
+    if (instance_id): attributes["service.instance.id"] = instance_id
+    resource = Resource.create(attributes)
     logger_provider = LoggerProvider(resource)
     set_logger_provider(logger_provider)
 
@@ -30,6 +34,6 @@ def configure(service_name: str | None, instance_id: str | None, api_key: str | 
     logging.getLogger().addHandler(LoggingHandler())
     configured = True
 
-def instrument_app(app, service_name: str | None = None, instance_id: str | None = None):
-    configure(service_name, instance_id)
-    FastAPIInstrumentor().instrument_app(app)
+def instrument_app(service_name: str | None = None, instance_id: str | None = None, api_key: str | None = env_api_key):
+    configure(service_name, instance_id, api_key)
+    logging.getLogger().setLevel(logging.DEBUG)
